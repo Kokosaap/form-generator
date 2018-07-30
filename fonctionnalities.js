ad6a7f5818e0e7ba69a26b3be98062862a6284d8
Kokosaap <leanaelle.salmier@epitech>
2018-07-30 17:53:00 +0200

    add input type option

--- fonctionnalities.js
+++ fonctionnalities.js
@@ -1,45 +1,56 @@
-// Le Label
+// Le Fieldset
 $('#leLabel').click(function(){
-  $('hr').after('<div id="container">Choisissez un Label <input type="text" id="idinput"><br/><br/><button id="btnok">Valider</button><button id="btnannuler">Annuler</button></div><div id="container2"></div>');
+  $('hr').after('<form><fieldset>',
+  '<div id="container">Choisissez un Label <input type="text" id="idinput"><br/><br/>',
+  '>Choisissez un type de champ <br/>',
+  '<input type="radio" id="password" name="pw" /><label for="password">password</label><br/>',
+  '<input type="radio" id="text" name="text" /><label for="text">Text</label>',
+  '<input type="radio" id="email" name="email" /><label for="email">Email</label>',
+  '</fieldset></form>',
+  '<button id="btnok">Valider</button><button id="btnannuler">Annuler</button>',
+  '</div><div id="container2"></div>',
+);
 
-  // Ajax
-  $('#container2').load('initialize.html .zlabel');
 
-  // Un clic puis on descative les btn principal jusqu'à ce qu'on remplisse le formulaire
-  $('.btnprincipal').attr('disabled','disabled');
-  $('#container').hide().fadeIn(1000);
-  $('#container2').hide().fadeIn(1000);
+// Ajax
+$('#container2').load('initialize.html .zlabel');
 
-  // Input
+// Un clic puis on descative les btn principal jusqu'à ce qu'on remplisse le formulaire
+$('.btnprincipal').attr('disabled','disabled');
+$('#container').hide().fadeIn(0.9);
+$('#container2').hide().fadeIn(0.9);
 
-  $("#idinput").focus();
+// Input
+$("#idinput").focus();
 
-  // Button Valider
-  $("#btnok").click(function(){
-    // La var est dans un span et on récupère la valeur de l'input
-    var validation = '<span>'+ $('#idinput').val() + '</span>&nbsp;<input type="text" id="' + $('#idinput').val() +'"><br/><br/>';
-    // On insère la valeur de validation dans la div #gauche
-    $('#gauche').append(validation);
-    // Si on click sur le bouton valider l'info est envoyé dans la div #gauche et le champs label s'en va
-    $('#container').fadeOut(1000, function(){
-      $('#container').remove();
-      // On supprime l'attibut 'disabled' inserer plus haut
-      // Si jamais on veut recréer un champs avec un Label
-      $('.btnprincipal').removeAttr('disabled');
-    });
-    $('#container2').fadeOut(1000);
+// Button Valider
+$("#btnok").click(function(){
+  // La var est dans un span et on récupère la valeur de l'input
+  var validation = '<span>'+ $('#idinput').val() + '</span><input type="text" id="' + $('#idinput').val() +'"><br>';
+  // On insère la valeur de validation dans la div #gauche
+  $('#gauche').append(validation);
+  // Si on click sur le bouton valider l'info est envoyé dans la div #gauche et le champs label s'en va
+  $('#container').fadeOut(0.9, function(){
+    $('#container').remove();
+    // On supprime l'attibut 'disabled' inserer plus haut
+    // Si jamais on veut recréer un champs avec un Label
+    $('.btnprincipal').removeAttr('disabled');
   });
+  $('#container2').fadeOut(0.9);
+});
 
-  // Button Annuler
-  $("#btnannuler").click(function(){
-    //Si on click sur le bouton annuler le champs label s'en va
-    $('#container').fadeOut(1000, function(){
-      $('#container').remove();
-      $('.btnprincipal').removeAttr('disabled');
-    });
-    $('#container2').fadeOut(1000);
+// Button Annuler
+$("#btnannuler").click(function(){
+  //Si on click sur le bouton annuler le champs label s'en va
+  $('#container').fadeOut(0.9, function(){
+    $('#container').remove();
+    $('.btnprincipal').removeAttr('disabled');
   });
+  $('#container2').fadeOut(0.9);
+});
 }); // Fin Label
+
+
 
 // Bouton
 $('#leBouton').click(function(){
@@ -50,11 +61,10 @@ $('#leBouton').click(function(){
 
   // Un clic puis on descative les btn principal jusqu'à ce qu'on remplisse le formulaire
   $('.btnprincipal').attr('disabled','disabled');
-  $('#container').hide().fadeIn(1000);
-  $('#container2').hide().fadeIn(1000);
+  $('#container').hide().fadeIn(0.9);
+  $('#container2').hide().fadeIn(0.9);
 
   // Input
-
   $("#idinput").focus();
 
   // Button Valider
@@ -64,70 +74,78 @@ $('#leBouton').click(function(){
     // On insère la valeur de validation dans la div #gauche
     $('#gauche').append(validation);
     // Si on click sur le bouton valider l'info est envoyé dans la div #gauche et le champs label s'en va
-    $('#container').fadeOut(1000, function(){
+    $('#container').fadeOut(0.9, function(){
       $('#container').remove();
       // On supprime l'attibut 'disabled' inserer plus haut
       // Si jamais on veut recréer un champs avec un Label
       $('.btnprincipal').removeAttr('disabled');
     });
-    $('#container2').fadeOut(1000);
+    $('#container2').fadeOut(0.9);
   });
 
   // Button Annuler
   $("#btnannuler").click(function(){
     //Si on click sur le bouton annuler le champs label s'en va
-    $('#container').fadeOut(1000, function(){
+    $('#container').fadeOut(0.9, function(){
       $('#container').remove();
       $('.btnprincipal').removeAttr('disabled');
     });
-    $('#container2').fadeOut(1000);
+    $('#container2').fadeOut(0.9);
   });
 }); // Fin bouton
 
+
+
 // Code Formulaire
 $('#cForm').click(function(){
-  $('hr').after('<div id="container">Copier le code du formulaire<br/><br/><button id="btnok">Valider</button><button id="btnannuler">Annuler</button></div><div id="container2"></div>');
+
+  // Input
+  $("#idinput").focus();
+
+  var htmlString = $('#gauche').html();
+  $('#code-form').text(htmlString);
+});
+// Fin code formulaire
+
+
+// Effacer le code
+$('#supprimer').click(function(){
+  $('hr').after('<div id="container">Supprimer le contenu<br/><br/><button id="btnok">Valider</button><button id="btnannuler">Annuler</button></div><div id="container2"></div>');
 
   // Ajax
-  $('#container2').load('initialize.html .zform');
+  $('#container2').load('initialize.html .zefface');
 
   // Un clic puis on descative les btn principal jusqu'à ce qu'on remplisse le formulaire
   $('.btnprincipal').attr('disabled','disabled');
-  $('#container').hide().fadeIn(1000);
-  $('#container2').hide().fadeIn(1000);
+  $('#container').hide().fadeIn(0.9);
+  $('#container2').hide().fadeIn(0.9);
 
   // Input
-
   $("#idinput").focus();
 
   // Button Valider
   $("#btnok").click(function(){
     // La var est dans un span et on récupère la valeur de l'input
-    var validation = '<button class="code">Affichage du code </button><br/>';
-    // On insère la valeur de validation dans la div #gauche
-    $('#code-form').append(validation);
+    $("#gauche, #code-form").empty();
+
     // Si on click sur le bouton valider l'info est envoyé dans la div #gauche et le champs label s'en va
-    $('#container').fadeOut(1000, function(){
+    $('#container').fadeOut(0.9, function(){
       $('#container').remove();
       // On supprime l'attibut 'disabled' inserer plus haut
       // Si jamais on veut recréer un champs avec un Label
       $('.btnprincipal').removeAttr('disabled');
     });
-    $('#container2').fadeOut(1000);
-    $('.code').click(function(){
-      var htmlString = $('#code-form').html();
-      $('#code-form').text(htmlString);
-      $('#code-form').append(htmlString);
-    });
+    $('#container2').fadeOut(0.9);
+
   });
 
   // Button Annuler
   $("#btnannuler").click(function(){
     //Si on click sur le bouton annuler le champs label s'en va
-    $('#container').fadeOut(1000, function(){
+    $('#container').fadeOut(0.9, function(){
       $('#container').remove();
       $('.btnprincipal').removeAttr('disabled');
     });
-    $('#container2').fadeOut(1000);
+    $('#container2').fadeOut(0.9);
   });
 }); // Fin code formulaire
