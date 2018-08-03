85a63376c0d887cf73b3004ee0546084768aa70d
Kokosaap <leanaelle.salmier@epitech>
2018-08-03 17:45:50 +0200

    input type ok

--- fonctionnalities.js
+++ fonctionnalities.js
@@ -1,6 +1,6 @@
 // Le Fieldset
 $('#leLabel').click(function(){
-  $('hr').after('<div id="container">Choisissez un Label <input type="text" id="idinput"><br/><br/>Choisissez le type du champs <input type="text" id="typeField"><br/><br/><button type="button" class="btn" id="btnok">Valider</button><button type="button" class="btn" id="btnannuler">Annuler</button></div><div id="container2"></div>');
+  $('hr').after('<div id="container">Choisissez un Label <input type="text" id="idinput"><br/><br/>Choisissez le type du champ <br/><input type="radio" id="typeField_txt" name="chooseType" checked /><label for="text">Text</label><br/><input type="radio" id="typeField_pswd" name="chooseType" /><label for="password">Password</label><br/><input type="radio" id="typeField_date" name="chooseType" /><label for="date">Date</label><br/><input type="radio" id="typeField_mail" name="chooseType" /><label for="email">Email</label><br/><input type="radio" id="typeField_nb" name="chooseType" value="number" /><label for="typeField_nb">Number</label><br/><br/><button type="button" class="btn" id="btnok">Valider</button><button type="button" class="btn" id="btnannuler">Annuler</button></div><div id="container2"></div>');
 
 
   // Ajax
@@ -17,7 +17,10 @@ $('#leLabel').click(function(){
   // Button Valider
   $("#btnok").click(function(){
     // La var est dans un span et on récupère la valeur de l'input
-    var validation = '<label>'+ $('#idinput').val() + '</label><input class="form-control" type="' + $('#typeField').val() + '" id="' + $('#idinput').val() +'"><br>';
+    var selectedProduct =$(  $(":radio[name=chooseType]:checked").prop("labels") ).text();
+    console.log(selectedProduct);
+    var validation = '<label>'+ $('#idinput').val() + '</label><input class="form-control" type="' + selectedProduct + '" id="' + $('#idinput').val() +'"><br>';
+
     // On insère la valeur de validation dans la div #gauche
     $('#gauche').append(validation);
     // Si on click sur le bouton valider l'info est envoyé dans la div #gauche et le champs label s'en va
